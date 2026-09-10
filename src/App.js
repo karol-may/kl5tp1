08958165034fa3c4c7f0f3058cb1b310758babb6
= <=>
2026-09-10 08:15:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Link, RedLink } from './components/Link';
+import { Table, TableHeader } from './components/Table';
+import { linkArray, linkArrayColumns } from './data/data';
+
+function App() {
+  return (
+    <div className="App">
+        <Table data={linkArray} columns={linkArrayColumns}/>
+        <hr/>
+        <Form/>
+    </div>
+  );
+}
+
+export default App;
